dc5958f8a2063e4936dc77cb65dbc8bd344a8273
v.shevchenko <v.shevchenko@plarium.com>
2023-06-22 19:30:49 +0300

    test prelender

--- Game Jam/Assets/Scripts/WindowsManager.cs
+++ Game Jam/Assets/Scripts/WindowsManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.Video;
 public class WindowsManager : MonoBehaviour
 {
   public GameObject Intro;
+  public GameObject Level1;
 
   void Start()
   {
     Intro.active = true;
+    Level1.active = false;
     var introVideo = Intro.GetComponentInChildren<VideoPlayer>();
     introVideo.Play();
     introVideo.loopPointReached += ShowLevel;
@@ -18,21 +20,27 @@ public class WindowsManager : MonoBehaviour
   public void ShowLevel(VideoPlayer vp)
   {
     vp.Stop();
-    Intro.active = false;
+    //Level1.active = true;
     //PreloaderAnimator.Instance.Play("Start_Level");
     //PreloaderAnimator.Instance.Play("Game_Over2");
-    PreloaderAnimator.Instance.Play("Start_Level2");
+
+    PreloaderAnimator.Instance.Play("Game_Over2");
+    //PreloaderAnimator.Instance.Play("Start_Level2");
+
+    Intro.active = false;
   }
 
   public void ToNextState()
   {
-    
-    
+
+    PreloaderAnimator.Instance.Play("Start_Level2");
+    Level1.active = true;
+
   }
 
   public void ShowEnd()
   {
-
-
+    PreloaderAnimator.Instance.Play("Start_Level2");
+    Level1.active = true;
   }
 }
